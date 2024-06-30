5897a7867f0260f43423cfa339ff69b706df9f6f
joaocleisson <joaocleisson@gmail.com>
2024-06-30 19:32:24 -0300

    Inserção da lista 2 de exercícios. Até o exercício número 6

--- primeira_lista_de_exercicios/Program.cs
+++ primeira_lista_de_exercicios/Program.cs
@@ -1,26 +1,107 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("<-------------------------Início do Exercício----------------------------->");
+Console.WriteLine("<----------------------------Exercício 1---------------------------------->");
 
 // 1.	Faça um Programa que mostre a mensagem "Alo mundo" na tela.
 
 Console.WriteLine("Alô, mundo!");
-
+Console.WriteLine("<----------------------------Exercício 2---------------------------------->");
 // 2.	Faça um Programa que peça um número e imprima seu dobro.
 
-// Perguntar o nome ao usuário, ler e armazenar na variável nome
+// Perguntar o nome ao usuário, ler e armazenar na variável user_insert
 Console.Write("Qual o seu nome? ");
-string nome = Console.ReadLine();
-// Pedir ao usuário para digitar um número, ler e armazenar na variável num
-Console.Write($"{nome}, digite um número: ");
-double num = double.Parse(Console.ReadLine());
-Console.WriteLine($"O dobro do número que você digitou é: {num * 2}");
+string? user_insert = Console.ReadLine();
+if(user_insert != null){
+    string nome = user_insert;
+    // Pedir ao usuário para digitar um número, ler e armazenar na variável num
+    Console.Write($"{nome}, digite um número: ");
+    double? num = double.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine($"O dobro do número que você digitou é: {num * 2}");
+} else {
+    Console.Write("É necessário digitar um nome para processir");
+    // Perguntar novamente o nome ao usuário, ler e armazenar na variável user_insert
+    Console.Write("Qual o seu nome? ");
+    string? user_insert2 = Console.ReadLine();
+    if(user_insert2 != null){
+    string nome = user_insert2;
+    // Pedir ao usuário para digitar um número, ler e armazenar na variável num
+    Console.Write($"{nome}, digite um número: ");
+    double num = double.Parse(Console.ReadLine() ?? "0");
+    Console.WriteLine($"O dobro do número que você digitou é: {num * 2}");
+    }else{
+        Console.Write("você não digitou um número válido, o programa será encerrado.");
+        Environment.Exit(0);
+    }
+}
 
+Console.WriteLine("<----------------------------Exercício 3---------------------------------->");
 // 3.	Faça um Programa que converta metros para centímetros. (1 metro tem quantos centímetros? Pense nisso).
 // Solicitar ao usuário que insira um valor a ser convertido
+Console.WriteLine("Agora você poderá digitar um valor em metros para saber o valor correspondente em centímetros. Vamos lá! ");
 Console.Write("Digite o valor em metros: ");
-double metros = double.Parse(Console.ReadLine());
+double metros = double.Parse(Console.ReadLine() ?? "0");
 
 // Realizar a conversão
 double centimetros = metros * 100;
 
 // Exibir resultado da conversão para o usuário
 Console.WriteLine($"{metros} metros equivalem a {centimetros} centímetros.");
+
+Console.WriteLine("<----------------------------Exercício 4---------------------------------->");
+// 4.	Escreva um algoritmo para ler as dimensões de um quadrado e mostre sua área (base * altura ou base ).
+Console.WriteLine("A seguir, digite o valor da base ou da altura de um quadrado e retornaremos a área dele. ");
+Console.Write("Digite um valor:");
+double base_altura = double.Parse(Console.ReadLine() ?? "0");
+if(base_altura == 0){
+    Console.WriteLine("Você digitou um valor nulo, portanto, não foi possível calcular a área do quadrado.");
+} else {
+    double area_quadrado = base_altura * base_altura;
+    Console.WriteLine($"A área do quadrado é {area_quadrado}");
+}
+
+Console.WriteLine("<----------------------------Exercício 5---------------------------------->");
+// 5.	Faça um Programa que peça o raio de um círculo, calcule e mostre sua área (area do círculo = 3.14 * raio ao quadrado).
+Console.WriteLine("A seguir, digite o valor do raio de um círculo e retornaremos a área dele. ");
+Console.Write("Digite um valor:");
+double raio = double.Parse(Console.ReadLine() ?? "0");
+if(raio == 0){
+    Console.WriteLine("Você digitou um valor nulo, portanto, não foi possível calcular a área do círculo.");
+} else{
+    double area_circulo = Math.Pow((raio * 3.14),2);
+    Console.WriteLine($"A área do círculo é {area_circulo}");
+}
+
+Console.WriteLine("<----------------------------Exercício 6---------------------------------->");
+// 6.	Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o valor, seu antecessor e seu sucessor (cada um em uma linha).
+Console.WriteLine("A seguir, digite um valor e retornaremos ele próprio, seu antecessor e seu sucessor. ");
+Console.Write("Digite um valor:");
+double? valor_digitado = double.Parse(Console.ReadLine());
+if(valor_digitado == null){
+    Console.WriteLine("Você não digitou um valor ou inseriu um valor nulo.");
+} else{
+    Console.WriteLine($"O valor que você digitou é: {valor_digitado}");
+    Console.WriteLine($"O antecessor do número digitado é: {--valor_digitado}");
+    Console.WriteLine($"O antecessor do número digitado é: {++valor_digitado + 1}");
+}
+
+Console.WriteLine("<----------------------------Exercício 7---------------------------------->");
+// 7.	Escreva um algoritmo para ler o salário mensal atual de um funcionário e calcular e escrever o valor do novo salário com 12% de reajuste.
+Console.WriteLine("Digite o valor do seu salário e lhe daremos 12% de aumento. Olha que maravilha!");
+Console.Write("Digite o valor do seu salário: ");
+double sal_inicial = double.Parse(Console.ReadLine() ?? "Você não digitou um valor de salário válido, não receberá seu aumento");
+if(sal_inicial > 0){
+    double sal_atualizado = (sal_inicial * 0.12) + sal_inicial;
+    Console.WriteLine($"Aqui está o novo valor do seu salário: {sal_atualizado}");
+}
+
+Console.WriteLine("<----------------------------Exercício 8---------------------------------->");
+// 8.	Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos. (1 minutos tem quantos segundos? 1 hora tem quantos minutos?) 
+
+Console.WriteLine("<----------------------------Exercício 9---------------------------------->");
+// 9.	Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo): 
+// Observação: Saiba que 100oC = 212F
+
+Console.WriteLine("<----------------------------Exercício 10--------------------------------->");
+// 10.	O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do 
+//distribuidor seja de 28% e os impostos de 45%, escrever um algoritmo para ler o custo de fábrica de um carro, calcular e escrever o custo final ao consumidor. 
+Console.WriteLine("<-----------------------------Fim do Exercício----------------------------------->");
---
+++ segunda_lista_de_exercicios/Program.cs
@@ -0,0 +1,173 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("<-------------------------Início do Exercício----------------------------->");
+
+Console.WriteLine("<----------------------------Exercício 1---------------------------------->");
+/* 1. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a 
+      idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias. */
+Console.Write("Digite seu nome: ");
+string? nome = Console.ReadLine();
+if(string.IsNullOrWhiteSpace(nome)){
+    Console.WriteLine("Nome digitado não é válido ou nulo. O programa será encerrado");
+    Environment.Exit(0);
+} else {
+     Console.Write($"{nome}, informe sua idade em anos: ");
+     string? idade_anos_digitada = Console.ReadLine();
+    /* 
+    O valor digitado tem que ser convertido para inteiro. A função TryParse retorna true para valor convertido e false para valor não convertido. 
+    Na condição do IF, quero testar se o usuário digitou valor negativo ou se não digitou valor nenhum.
+    Tive que utilizar o operador de negação para conseguir entrar no laço corretamente,
+    pois se digitar um valor válido (que não seja letras ou nulo) ele convertido com sucesso e a condição de validação do TryParse será atendida, 
+    retornando true, mas esse true afetará a condição de validação do IF, que espera um true para qualquer das condições testadas.
+    */
+    if(!int.TryParse(idade_anos_digitada, out int idade_anos) || idade_anos < 0){
+        Console.WriteLine("A idade digitada não é válida para continuação do programa. A execução será finalizada.");
+        Environment.Exit(0);
+    }
+    Console.Write($"{nome}, informe a quantidade de meses posteriores a sua idade completa em anos (Exemplo: 10 anos e 7 meses, digite apenas 7): ");
+    string? idade_meses_digitada = Console.ReadLine();
+    if(!int.TryParse(idade_meses_digitada, out int idade_meses) || idade_meses < 0 || idade_meses > 11){
+        Console.WriteLine("A idade digitada não é válida para continuação do programa. A execução será finalizada.");
+        Environment.Exit(0);
+    }
+    Console.Write($"{nome}, informe a quantidade de dias posteriores a sua idade completa em anos (Exemplo: 10 anos, 7 meses e 20 dias, digite apenas 20): ");
+    string? idade_dias_digitada = Console.ReadLine();
+    if(!int.TryParse(idade_dias_digitada, out int idade_dias) || idade_dias < 0 || idade_dias > 30){
+        Console.WriteLine("A idade digitada não é válida para continuação do programa. A execução será finalizada.");
+        Environment.Exit(0);
+    }
+    int idade_em_dias = (idade_anos * 365) + (idade_meses * 30) + idade_dias;
+    Console.WriteLine($"Sua idade em dias é: {idade_em_dias}");
+} 
+
+Console.WriteLine("<----------------------------Exercício 2---------------------------------->");
+// 2. Faça um Programa que peça 4 notas e mostre a média (10 + 10 + 10 + 10 / 4 tem que dar resultado 10).
+Console.WriteLine($"{nome}, agora você poderá digitar 4 notas e retornaremos sua nota média.");
+Console.Write("Digite a primeira nota: ");
+string? nota1_digitada = Console.ReadLine();
+if (!double.TryParse(nota1_digitada, out double nota1) || nota1 < 0 || nota1 > 10){
+    Console.WriteLine("O valor digitado é inválido. O programa será encerrado.");
+    Environment.Exit(0);
+}
+Console.Write("Digite a segunda nota: ");
+string? nota2_digitada = Console.ReadLine();
+if (!double.TryParse(nota2_digitada, out double nota2) || nota2 < 0 || nota2 > 10){
+    Console.WriteLine("O valor digitado é inválido. O programa será encerrado.");
+    Environment.Exit(0);
+}
+Console.Write("Digite a terceira nota: ");
+string? nota3_digitada = Console.ReadLine();
+if (!double.TryParse(nota3_digitada, out double nota3) || nota3 < 0 || nota3 > 10){
+    Console.WriteLine("O valor digitado é inválido. O programa será encerrado.");
+    Environment.Exit(0);
+}
+Console.Write("Digite a quarta nota: ");
+string? nota4_digitada = Console.ReadLine();
+if (!double.TryParse(nota4_digitada, out double nota4) || nota4 < 0 || nota4 > 10){
+    Console.WriteLine("O valor digitado é inválido. O programa será encerrado.");
+    Environment.Exit(0);
+}
+Console.WriteLine($"A média da sua nota é: {(nota1+nota2+nota3+nota4)/4}");
+
+Console.WriteLine("<----------------------------Exercício 3---------------------------------->");
+//3. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+Console.WriteLine($"{nome}, digite o valor da base ou da altura de um quadrado e retornaremos a área e o dobro da área dele. ");
+Console.Write("Digite um valor:");
+string? valor_digitado = Console.ReadLine();
+if (!double.TryParse(valor_digitado, out double valor) || valor <= 0){
+    Console.WriteLine("O valor digitado é inválido. O programa será encerrado.");
+    Environment.Exit(0);
+}
+double area_quadrado = valor * valor;
+Console.WriteLine($"A área do quadrado é {area_quadrado}");
+Console.WriteLine($"O dobro da área do quadrado é {area_quadrado * 2}");
+
+Console.WriteLine("<----------------------------Exercício 4---------------------------------->");
+// 4. Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. 
+//  Calcule o total em segundos. (1 minutos tem quantos segundos? 1 hora tem quantos minutos?)
+
+Console.Write($"{nome}, digite uma quantidade de dias, horas, minutos e segundos nas opções abaixo que retornar em segundos.");
+Console.Write("Digite uma quantidade de dias: ");
+string? dias_digitado = Console.ReadLine();
+if(!int.TryParse(dias_digitado, out int dias) || dias < 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+Console.Write("Digite uma quantidade de horas: ");
+string? horas_digitada = Console.ReadLine();
+if (!int.TryParse(horas_digitada, out int horas) || horas <= 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+Console.Write("Digite uma quantidade de minutos: ");
+string? minutos_digitado = Console.ReadLine();
+if (!int.TryParse(minutos_digitado, out int minutos) || minutos < 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+Console.Write("Digite uma quantidade de segundos: ");
+string? segundos_digitado = Console.ReadLine();
+if (!int.TryParse(segundos_digitado, out int segundos) || segundos < 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+int total_segundos = (dias * 24 * 60 * 60) + (horas * 60 * 60) + (minutos * 60) + segundos;
+Console.WriteLine($"O total de segundos, considerando todos os valores digitados, é: {total_segundos}");
+
+Console.WriteLine("<----------------------------Exercício 5---------------------------------->");
+//5. Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+Console.WriteLine($"{nome}, vamos calcular a quantidade de horas de uma viagem baseada no KM e velocidade");
+Console.Write("Digite a distância: ");
+string? distancia_digitada = Console.ReadLine();
+if (!double.TryParse(distancia_digitada, out double distancia) || distancia <= 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+Console.Write("Digite a velocidade média: ");
+string? velocidade_digitada = Console.ReadLine();
+if (!double.TryParse(velocidade_digitada, out double velocidade) || velocidade <= 0){
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+Console.WriteLine($"O tempo estimado para viagem é de {distancia / velocidade} horas.");
+
+Console.WriteLine("<----------------------------Exercício 6---------------------------------->");
+// 6.	Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade 
+//      de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos de vida a cada cigarro,
+//      calcule quantos dias de vida um fumante perderá. Exiba o total de dias.
+
+Console.WriteLine($"{nome}, você é fumante? Digite 'S' para sim ou 'N' para não");
+string? resposta = Console.ReadLine();
+if (resposta == "S" || resposta == "s"){
+    Console.Write("Há quanto anos você fuma? ");
+    string? tempo = Console.ReadLine();
+    if (!int.TryParse(tempo, out int anos) || anos <= 0){
+        Console.Write("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+        Environment.Exit(0);
+    }
+    Console.WriteLine("Quantos cigarros você fuma por dia? ");
+    string? quantidade = Console.ReadLine();
+    if (!int.TryParse(quantidade, out int qtde) || qtde <= 0){
+        Console.Write("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+        Environment.Exit(0);
+    }
+    int perda_dia = qtde * 10;
+    int perda_minutos = anos * 365 * perda_dia;
+    double perda_total = perda_minutos / 60 / 24;
+    Console.WriteLine($" Você perdeu {perda_total} dias da sua vida.");
+} else if (resposta == "N" || resposta == "n"){
+    Console.WriteLine("Parabéns! você não está perdendo dias de vida.");
+} else {
+    Console.WriteLine("O valor digitado não é válido para continuação do programa. A execução será finalizada.");
+    Environment.Exit(0);
+}
+
+Console.WriteLine("<----------------------------Exercício 7---------------------------------->");
+
+Console.WriteLine("<----------------------------Exercício 8---------------------------------->");
+
+Console.WriteLine("<----------------------------Exercício 9---------------------------------->");
+
+Console.WriteLine("<----------------------------Exercício 10--------------------------------->");
+
+Console.WriteLine("<--------------------------Fim do Exercício------------------------------->");
+
