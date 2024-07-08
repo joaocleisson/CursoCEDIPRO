f9debd16297436071b1dcb0be149a0ce40949b35
joaocleisson <joaocleisson@gmail.com>
2024-07-07 22:26:10 -0300

    Exercicio 4

--- Lista 3/exercicio_04/Program.cs
+++ Lista 3/exercicio_04/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+Console.Write("Digite uma letra: ");
+char letra = char.Parse(Console.ReadLine());
+if(letra == 'a' || letra == 'A' || letra == 'e' || letra == 'E' || letra == 'i' || letra == 'I' || letra == 'o' || letra == 'O' || letra == 'u' || letra == 'U'){
+    Console.WriteLine("Vogal");
+} else {
+    Console.WriteLine("Consoante");
+}
